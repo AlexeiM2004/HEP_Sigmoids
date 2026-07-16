aecd39ee5f5c942ea5a655ee919291d5bdaa66fa
Xand711 <charles.bamsey.@student.manchester.ac.uk>
2026-07-16 10:26:23 +0100

    Created a basic flowmatching model

---
+++ flowmatch_submit.sh
@@ -0,0 +1,27 @@
+#!/bin/bash --login
+#SBATCH -p gpuL
+#SBATCH -G 1
+#SBATCH -t 1-0
+#SBATCH -c 12
+
+echo "# ---------- BATCH RECIEPT ---------- #"
+echo "Job ID: $SLURM_JOB_ID"
+echo "Job Name: $SLURM_JOB_NAME"
+echo "Submit Time: $(date)"
+echo "Node: $(hostname)"
+echo "User: $USER"
+echo "Partition: $SLURM_JOB_PARTITION"
+echo "GPUs: $SLURM_GPUS"
+echo "Working Directory: $(pwd)"
+echo "# ----------------------------------- #"
+echo ""
+
+
+cp /mnt/iusers01/fse-ugpgt01/phy01/q44882cb/HEP_Sigmoids/train_inputs/{larger_ttbar_train.h5,larger_ttbar_test.h5,larger_ttbar_val.h5,larger_scaler_info.h5,feature_labels.h5} ~/scratch/train_inputs/
+
+cp /mnt/iusers01/fse-ugpgt01/phy01/q44882cb/HEP_Sigmoids/flowmatching.py ~/scratch/Scripts/
+
+cd ~/scratch/Scripts
+source ~/.venv/bin/activate
+
+python -u flowmatching.py
